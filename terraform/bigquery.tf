d1dfd2cb97132777330d4b669f2beee7a19a6a56
Sascha Dittmann <Sascha5812@gmx.de>
2025-06-12 14:12:05 +0200

    chore: remove duplicate bq connection

--- terraform/bigquery.tf
+++
@@ -1,6 +0,0 @@
-resource "google_bigquery_connection" "connection" {
-  connection_id = "masterclass"
-  location      = "US"
-  description   = "A connection to Vertex AI resources"
-  cloud_resource {}
-}
